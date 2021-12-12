ce0bae7446a013289af519b5782daa6582819f00
Ashifkhan04 <95920746+Ashifkhan04@users.noreply.github.com>
2021-12-12 12:42:29 +0530

    Added new file

---
+++ 1.py
@@ -0,0 +1,20 @@
+def myfunction(num1, num2):
+    product = num1 * num2
+    if product <= 1000:
+        return product
+    else:
+        num1 + num2
+        return num1 + num2
+
+print("first", myfunction(20, 30))
+print("second result", myfunction(40, 30))
+
+
+
+
+
+
+
+
+
+
